bc4e1cc9b442e008306a185b65d2d6ef07955ed2
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-02-11 07:58:44 +0900

    Update pos-system.py

--- pos-system.py
+++ pos-system.py
@@ -1,7 +1,6 @@
 ### 商品クラス
 from re import I
 
-
 class Item:
 
     def __init__(self,item_code,item_name,price):
@@ -9,53 +8,43 @@ class Item:
         self.item_name=item_name
         self.price=price
 
-    
     def GetItem_code(self):
         return self.item_code
-    
-    def GetItem_name(self):
-        return self.item_name
-    
-    def get_price(self):
-        return self.price
 
 ### オーダークラス
 class Order:
     # コンストラクタ　
-    def __init__(self,item_master):
+    def __init__(self,item_masters):
         self.item_order_list=[]
-        self.item_master=item_master
+        self.item_masters=item_masters
     
-    # def add_item_order(self):
-    #     # item_order_listにitem_name,priceを格納するために追記
-    #     self.item_order_list.append(self.item_master[0])
-    #     # self.item_order_list.append(item_name)
-    #     # self.item_order_list.append(price)
-    def Get_Item_Info(self,item_code):
-        for master in self.item_master:
+   
+    def Order_Items_mes(self,item_code):
+        # インスタンスのitem_mastersから、１つずつ値を取り出し、引数の商品コードと符合する商品を探す。
+        # その商品の、ID・名前・価格をprintメソッドで表示させる。
+        for master in self.item_masters:
             if item_code==master.item_code:
-                return master.item_name,master.price
-    
-    def view_item_list(self):
-        for item in self.item_order_list:
-            print("商品コード:{}".format(item))
+                print("商品コード:{},商品名:{},価格:{}".format(master.item_code,master.item_name,master.price))
+
 
 ### メイン処理　関数main
 def main():
 
-    item_master=[]
-    item_master.append(Item("001","りんご",100))
-    item_master.append(Item("002","なし",120))
-    item_master.append(Item("003","みかん",150))
+    # この店の商品(item)一覧情報をここで取り込み。
+    # ここではまだorderクラスはインスタンス化してない。
+    item_masters=[]
+    item_masters.append(Item("001","りんご",100))
+    item_masters.append(Item("002","なし",120))
+    item_masters.append(Item("003","みかん",150))
 
-    # 注文したcode、name,priceをaddしていく。item_order_listにaddされる。
-    order=Order(item_master)
-    order.add_item_order("001","りんご",100)
-    
-   
-    order.view_item_list()
+    # orderクラスをインスタンス化。Order_Item_Infoメソッドを使えるようにする。
+    # 商品コードを引数入力したら、ID・名前・価格をprintメソッドで表示させる。
+    order=Order(item_masters)
+    order_mes=input("商品コードを入力してください:")
+    order.Order_Items_mes(order_mes)
+    # order.Order_Item_Info("002")
+    # order.Order_Item_Info("003")
+
     
 if __name__ == "__main__":
     main()
-    
-    
