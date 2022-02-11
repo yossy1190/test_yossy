687104eefb26576a09f0650c09b81b92a093c8b4
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-02-11 11:55:04 +0900

    Update pos-system.py

--- pos-system.py
+++ pos-system.py
@@ -1,5 +1,5 @@
 ### 商品クラス
-from re import I
+
 
 class Item:
 
@@ -8,7 +8,7 @@ class Item:
         self.item_name=item_name
         self.price=price
 
-    def GetItem_code(self):
+    def getitem_code(self):
         return self.item_code
 
 ### オーダークラス
@@ -18,8 +18,10 @@ class Order:
         self.item_order_list=[]
         self.item_masters=item_masters
     
+
+        
    
-    def Order_Items_mes(self,item_code):
+    def print_order_item(self,item_code):
         # インスタンスのitem_mastersから、１つずつ値を取り出し、引数の商品コードと符合する商品を探す。
         # その商品の、ID・名前・価格をprintメソッドで表示させる。
         for master in self.item_masters:
@@ -31,7 +33,7 @@ class Order:
 def main():
 
     # この店の商品(item)一覧情報をここで取り込み。
-    # ここではまだorderクラスはインスタンス化してない。
+    # ここではまだorderクラスはインスタンス化していない。
     item_masters=[]
     item_masters.append(Item("001","りんご",100))
     item_masters.append(Item("002","なし",120))
@@ -40,11 +42,22 @@ def main():
     # orderクラスをインスタンス化。Order_Item_Infoメソッドを使えるようにする。
     # 商品コードを引数入力したら、ID・名前・価格をprintメソッドで表示させる。
     order=Order(item_masters)
-    order_mes=input("商品コードを入力してください:")
-    order.Order_Items_mes(order_mes)
-    # order.Order_Item_Info("002")
-    # order.Order_Item_Info("003")
-
+    
+    #買い物かご(order_lists)を作成
+    #inputによりコンソール画面で、商品コードを入力させる。
+    # while文を使って、注文したものをappendし続ける。
+    order_lists=[]
+    while True:
+        val=input("商品コードを入力(001～999)。Enterのみで精算します。:")
+        if val:
+            order_lists.append(val)
+        else:
+            break
+    
+    #order_listsをfor文で回して、inputで入力させた商品コードを変数itemに代入。
+    #変数itemを、print_order_itemメソッドに当てる。
+    for item in order_lists:
+        order.print_order_item(item)
     
 if __name__ == "__main__":
     main()
